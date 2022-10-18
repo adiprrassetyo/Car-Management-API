591543f51c8c37bee79b2b00ac06a5afbcc016ff
adeeep14 <disconetct@gmail.com>
2022-10-19 02:18:28 +0700

    add server and anything

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(cookieParser());
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
